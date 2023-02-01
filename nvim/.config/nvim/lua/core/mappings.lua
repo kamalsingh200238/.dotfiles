7f8cccc59da184aa8b03b8f2e31a5219dd31d412
Kamal Singh <kamalsingh200238@gmail.com>
2023-02-02 00:27:56 +0530

    changed diagnostic mappings

--- nvim/.config/nvim/lua/core/mappings.lua
+++ nvim/.config/nvim/lua/core/mappings.lua
@@ -15,11 +15,8 @@ map("n", "<leader>w", "<cmd>w<Cr>", { desc = "save file" }, opts)
 map("n", "<ESC>", "<cmd>noh<Cr>", { desc = "remove search highligting" }, opts)
 map("n", "+", "<C-a>", { desc = "increase count" }, opts)
 map("n", "-", "<C-x>", { desc = "decrease count" }, opts)
-
 map("n", "<C-a>", "gg<S-v>G", { desc = "select whole document" }, opts)
-
 map("n", "<leader><leader>s", "<cmd>source %<cr>", { desc = "source current file" }, opts)
-
 -- for better copy and paste
 map({ "n", "v" }, "<leader>y", '"+y', { desc = "copy in clipboard" }, opts)
 
@@ -28,38 +25,14 @@ map("n", "ss", ":split<Return><C-w>w", { desc = "split window horizontally" })
 map("n", "sv", ":vsplit<Return><C-w>w", { desc = "split vertically" }, opts)
 
 -- Move window
-map("n", "<C-h>", function()
-	require("smart-splits").move_cursor_left()
-end, { desc = "change panel to left" }, opts)
-
-map("n", "<C-l>", function()
-	require("smart-splits").move_cursor_right()
-end, { desc = "change panel to right" }, opts)
-
-map("n", "<C-j>", function()
-	require("smart-splits").move_cursor_down()
-end, { desc = "change panel to up" }, opts)
-
-map("n", "<C-k>", function()
-	require("smart-splits").move_cursor_up()
-end, { desc = "change panel to down" }, opts)
-
-map("n", "H", function()
-	require("smart-splits").move_cursor_left()
-end, { desc = "change panel to left" }, opts)
-
-map("n", "L", function()
-	require("smart-splits").move_cursor_right()
-end, { desc = "change panel to right" }, opts)
-
-map("n", "J", function()
-	require("smart-splits").move_cursor_down()
-end, { desc = "change panel to up" }, opts)
-
-map("n", "K", function()
-	require("smart-splits").move_cursor_up()
-end, { desc = "change panel to down" }, opts)
--- map('n', '<Space>', '<C-w>w',opts)
+map("n", "<C-h>", function() require("smart-splits").move_cursor_left() end, { desc = "change panel to left" }, opts)
+map("n", "<C-l>", function() require("smart-splits").move_cursor_right() end, { desc = "change panel to right" }, opts)
+map("n", "<C-j>", function() require("smart-splits").move_cursor_down() end, { desc = "change panel to up" }, opts)
+map("n", "<C-k>", function() require("smart-splits").move_cursor_up() end, { desc = "change panel to down" }, opts)
+map("n", "H", function() require("smart-splits").move_cursor_left() end, { desc = "change panel to left" }, opts)
+map("n", "L", function() require("smart-splits").move_cursor_right() end, { desc = "change panel to right" }, opts)
+map("n", "J", function() require("smart-splits").move_cursor_down() end, { desc = "change panel to up" }, opts)
+map("n", "K", function() require("smart-splits").move_cursor_up() end, { desc = "change panel to down" }, opts)
 -- map("n", "<C-h>", "<C-w>h", { desc = "change panel to left" }, opts)
 -- map("n", "<C-l>", "<C-w>l", { desc = "change panel to right" }, opts)
 -- map("n", "<C-j>", "<C-w>j", { desc = "change panel to up" }, opts)
@@ -70,21 +43,10 @@ end, { desc = "change panel to down" }, opts)
 -- map("n", "K", "<C-w>k", { desc = "change panel to down" }, opts)
 
 -- Resize window
-map("n", "<C-up>", function()
-	require("smart-splits").resize_up()
-end, { desc = "resize window" }, opts)
-
-map("n", "<C-down>", function()
-	require("smart-splits").resize_down()
-end, { desc = "resize window" }, opts)
-
-map("n", "<C-left>", function()
-	require("smart-splits").resize_left()
-end, { desc = "resize window" }, opts)
-
-map("n", "<C-right>", function()
-	require("smart-splits").resize_right()
-end, { desc = "resize window" }, opts)
+map("n", "<C-up>", function() require("smart-splits").resize_up() end, { desc = "resize window" }, opts)
+map("n", "<C-down>", function() require("smart-splits").resize_down() end, { desc = "resize window" }, opts)
+map("n", "<C-left>", function() require("smart-splits").resize_left() end, { desc = "resize window" }, opts)
+map("n", "<C-right>", function() require("smart-splits").resize_right() end, { desc = "resize window" }, opts)
 
 -- Packer
 -- map("n", "<leader>ps", "<cmd>PackerSync<Cr>", { desc = "packer sync" }, opts)
@@ -102,78 +64,24 @@ map("n", "<leader>e", "<cmd>NvimTreeToggle<Cr>", { desc = "Toggle Nvim tree" }, 
 map("n", "<leader>o", "<cmd>NvimTreeFocus<Cr>", { desc = "Focus Nvim tree" }, opts)
 
 -- Diagnostics
-map("n", "[d", "<Cmd>Lspsaga diagnostic_jump_prev<CR>", { desc = "jump prev diagonstic" }, opts)
-map("n", "]d", "<Cmd>Lspsaga diagnostic_jump_next<CR>", { desc = "jump next diagonstic" }, opts)
+map("n", "[d", function () vim.diagnostic.goto_prev() end, { desc = "jump prev diagonstic" }, opts)
+map("n", "]d", function () vim.diagnostic.goto_next() end, { desc = "jump next diagonstic" }, opts)
 
 -- For telescope
-map("n", "<leader>ft", function()
-	require("telescope.builtin").colorscheme()
-end, { desc = "change colorscheme" }, opts)
-
-map("n", "<leader>ff", function()
-	require("telescope.builtin").find_files()
-end, { desc = "Search files" }, opts)
-
-map("n", "<leader>fF", function()
-	require("telescope.builtin").find_files({ hidden = true, no_ignore = true })
-end, { desc = "Search all files" }, opts)
-
-map("n", "<leader>fw", function()
-	require("telescope.builtin").live_grep()
-end, { desc = "Search words" }, opts)
-
-map("n", "<leader>fW", function()
-	require("telescope.builtin").live_grep({
-		additional_args = function(args)
-			return vim.list_extend(args, { "--hidden", "--no-ignore" })
-		end,
-	})
-end, { desc = "Search words in all files" }, opts)
-
-map("n", "<leader>gs", function()
-	require("telescope.builtin").git_status()
-end, { desc = "Git status" }, opts)
-
-map("n", "<leader>gb", function()
-	require("telescope.builtin").git_branches()
-end, { desc = "Git branches" }, opts)
-
-map("n", "<leader>fb", function()
-	require("telescope.builtin").buffers()
-end, { desc = "search buffers" }, opts)
-
-map("n", "<leader>fh", function()
-	require("telescope.builtin").help_tags()
-end, { desc = "Search help" }, opts)
-
-map("n", "<leader>fm", function()
-	require("telescope.builtin").marks()
-end, { desc = "Search marks" }, opts)
-
-map("n", "<leader>fo", function()
-	require("telescope.builtin").oldfiles()
-end, { desc = "Search history" }, opts)
-
-map("n", "<leader>fc", function()
-	require("telescope.builtin").grep_string()
-end, { desc = "Search for word under cursor" }, opts)
-
-map("n", "<leader>fM", function()
-	require("telescope.builtin").man_pages()
-end, { desc = "Search man" }, opts)
-
-map("n", "<leader>fr", function()
-	require("telescope.builtin").registers()
-end, { desc = "Search registers" }, opts)
-
-map("n", "<leader>fk", function()
-	require("telescope.builtin").keymaps()
-end, { desc = "Search keymaps" }, opts)
-
-map("n", "<leader>fC", function()
-	require("telescope.builtin").commands()
-end, { desc = "Search commands" }, opts)
-
-map("n", "<leader>fd", function()
-	require("telescope.builtin").diagnostics()
-end, { desc = "Search diagnostics" }, opts)
+map("n", "<leader>ft", function() require("telescope.builtin").colorscheme() end, { desc = "change colorscheme" }, opts)
+map("n", "<leader>ff", function() require("telescope.builtin").find_files() end, { desc = "Search files" }, opts)
+map("n", "<leader>fF", function() require("telescope.builtin").find_files({ hidden = true, no_ignore = true }) end, { desc = "Search all files" }, opts)
+map("n", "<leader>fw", function() require("telescope.builtin").live_grep() end, { desc = "Search words" }, opts)
+map("n", "<leader>fW", function() require("telescope.builtin").live_grep({ additional_args = function(args) return vim.list_extend(args, { "--hidden", "--no-ignore" }) end, }) end, { desc = "Search words in all files" }, opts)
+map("n", "<leader>gs", function() require("telescope.builtin").git_status() end, { desc = "Git status" }, opts)
+map("n", "<leader>gb", function() require("telescope.builtin").git_branches() end, { desc = "Git branches" }, opts)
+map("n", "<leader>fb", function() require("telescope.builtin").buffers() end, { desc = "search buffers" }, opts)
+map("n", "<leader>fh", function() require("telescope.builtin").help_tags() end, { desc = "Search help" }, opts)
+map("n", "<leader>fm", function() require("telescope.builtin").marks() end, { desc = "Search marks" }, opts)
+map("n", "<leader>fo", function() require("telescope.builtin").oldfiles() end, { desc = "Search history" }, opts)
+map("n", "<leader>fc", function() require("telescope.builtin").grep_string() end, { desc = "Search for word under cursor" }, opts)
+map("n", "<leader>fM", function() require("telescope.builtin").man_pages() end, { desc = "Search man" }, opts)
+map("n", "<leader>fr", function() require("telescope.builtin").registers() end, { desc = "Search registers" }, opts)
+map("n", "<leader>fk", function() require("telescope.builtin").keymaps() end, { desc = "Search keymaps" }, opts)
+map("n", "<leader>fC", function() require("telescope.builtin").commands() end, { desc = "Search commands" }, opts)
+map("n", "<leader>fd", function() require("telescope.builtin").diagnostics() end, { desc = "Search diagnostics" }, opts)
