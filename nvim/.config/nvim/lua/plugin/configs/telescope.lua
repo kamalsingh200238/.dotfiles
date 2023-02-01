c4739188b9f819c2f2db6f119fa9e84d5b9627fc
Kamal Singh <kamalsingh200238@gmail.com>
2023-02-01 18:54:09 +0530

    changed setting

--- nvim/.config/nvim/lua/plugin/configs/telescope.lua
+++ nvim/.config/nvim/lua/plugin/configs/telescope.lua
@@ -1,41 +1,41 @@
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
-  print("telescope not installed")
-  return
+	print("telescope not installed")
+	return
 end
 
 local actions = require("telescope.actions")
 
 telescope.setup({
-  defaults = {
-    prompt_prefix = "   ",
-    selection_caret = "  ",
-    entry_prefix = "  ",
-    sorting_strategy = "ascending",
-    layout_config = {
-      horizontal = {
-        preview_width = 0.5,
-        results_width = 0.5,
-      },
-      prompt_position = "top",
-      width = 0.90,
-      height = 0.99,
-      preview_cutoff = 120,
-    },
-    file_ignore_patterns = { "node_modules" },
-    borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-    color_devicons = true,
-  },
-  pickers = {
-    colorscheme = {
-      enable_preview = true,
-    },
-  },
-  mappings = {
-    i = {
-      ["<C-j>"] = actions.cycle_history_next,
-      ["<C-k>"] = actions.cycle_history_prev,
-    },
-    n = { ["q"] = actions.close },
-  },
+	defaults = {
+		prompt_prefix = "   ",
+		selection_caret = "  ",
+		entry_prefix = "  ",
+		sorting_strategy = "ascending",
+		layout_config = {
+			horizontal = {
+				prompt_position = "top",
+				preview_width = 0.5,
+				results_width = 0.5,
+			},
+			width = 0.90,
+			height = 0.99,
+			preview_cutoff = 120,
+		},
+		file_ignore_patterns = { "node_modules" },
+		borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+		color_devicons = true,
+	},
+	pickers = {
+		colorscheme = {
+			enable_preview = true,
+		},
+	},
+	mappings = {
+		i = {
+			["<C-j>"] = actions.cycle_history_next,
+			["<C-k>"] = actions.cycle_history_prev,
+		},
+		n = { ["q"] = actions.close },
+	},
 })
