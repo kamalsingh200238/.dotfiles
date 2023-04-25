cd7b70752858141b0635077c1485fa6362777c58
Kamal Singh <kamalsingh200238@gmail.com>
2023-04-25 18:00:16 +0530

    feat: Add colorscheme and hop

--- nvim/.config/nvim/lua/user/plugins/user.lua
+++ nvim/.config/nvim/lua/user/plugins/user.lua
@@ -2,6 +2,19 @@ return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
+  { "rose-pine/neovim", event = "VeryLazy" },
+  { "catppuccin/nvim", event = "VeryLazy" },
+  { "EdenEast/nightfox.nvim", event = "VeryLazy" },
+  { "sainnhe/gruvbox-material", event = "VeryLazy" },
+  { "LunarVim/horizon.nvim", event = "VeryLazy" },
+  { "olimorris/onedarkpro.nvim", event = "VeryLazy" },
+  {
+    "phaazon/hop.nvim",
+    branch = "v2",
+    event = "VeryLazy",
+    opts = {},
+  },
+
   -- {
   --   "ray-x/lsp_signature.nvim",
   --   event = "BufRead",
