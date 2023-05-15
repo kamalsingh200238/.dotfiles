b3d58872fa208667ba230e5ebb133eedd6970825
Kamal Singh <kamalsingh200238@gmail.com>
2023-05-15 17:37:04 +0530

    chore(nvim): Change colorshceme

--- nvim/.config/nvim/lua/user/init.lua
+++ nvim/.config/nvim/lua/user/init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  colorscheme = "rose-pine-moon",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
