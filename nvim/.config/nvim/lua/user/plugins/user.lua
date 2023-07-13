98e148167acc49344be68bb9ea06e0e6e7b6b825
Kamal Singh <kamalsingh200238@gmail.com>
2023-07-13 16:18:19 +0530

    fix: Remove unused plugins

--- nvim/.config/nvim/lua/user/plugins/user.lua
+++ nvim/.config/nvim/lua/user/plugins/user.lua
@@ -7,8 +7,6 @@ return {
   { "EdenEast/nightfox.nvim", event = "VeryLazy" },
   { "sainnhe/gruvbox-material", event = "VeryLazy" },
   { "olimorris/onedarkpro.nvim", event = "VeryLazy" },
-  { "rktjmp/lush.nvim", event = "VeryLazy" },
-  { dir = "~/projects/colorschemes/", event = "VeryLazy" },
   -- { "notken12/base46-colors", event = "VeryLazy" },
   {
     "phaazon/hop.nvim",
