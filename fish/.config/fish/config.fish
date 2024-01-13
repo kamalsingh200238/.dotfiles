02b3a9a03f49180d2eedc128d942ccee32425541
Kamal Singh <kamalsingh200238@gmail.com>
2024-01-13 16:49:45 +0000

    feat: Added fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_cursor_insert line
+set fish_cursor_default block
+set fish_cursor_visual block
+set fish_cursor_replace underscore
+set fish_vi_force_cursor 1
+
+starship init fish | source
+zoxide init fish | source
