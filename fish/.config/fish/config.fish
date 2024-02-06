5579a74f3d090cc98035386795b997d4064ef95b
Kamal Singh <kamalsingh200238@gmail.com>
2024-02-06 13:03:17 +0000

    fix: match my zshrc file

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,5 +8,11 @@ set fish_cursor_visual block
 set fish_cursor_replace underscore
 set fish_vi_force_cursor 1
 
+set -gx EDITOR nvim
+set -gx VISUAL nvim 
+set -gx PATH $HOME/go/bin $HOME/.local/bin $PATH
+
+alias vil "env NVIM_APPNAME=lazyvim nvim"
+
 starship init fish | source
 zoxide init fish | source
