9a0ec457b052380289bbc5a36a1e6892e829a2f2
Kamal Singh <kamalsingh200238@gmail.com>
2024-03-31 14:48:40 +0100

    pnpm in path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,9 +10,16 @@ set fish_vi_force_cursor 1
 
 set -gx EDITOR nvim
 set -gx VISUAL nvim 
-set -gx PATH $HOME/go/bin $HOME/.local/bin $HOME/Library/pnpm $PATH
+set -gx PATH $HOME/go/bin $HOME/.local/bin $PATH
 
 alias vil "env NVIM_APPNAME=lazyvim nvim"
 
 starship init fish | source
 zoxide init fish | source
+
+# pnpm
+set -gx PNPM_HOME "/home/kg/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
