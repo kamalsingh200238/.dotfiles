ccd7fa991032c3eb1ddc897762578bdd72694235
Kamal Singh <kamal.singh@exa.net.uk>
2024-04-07 20:59:06 +0100

    add colorscheme

--- nvim/.config/nvim/lua/plugins/astroui.lua
+++ nvim/.config/nvim/lua/plugins/astroui.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroUI provides the basis for configuring the AstroNvim User Interface
 -- Configuration documentation can be found with `:h astroui`
@@ -11,7 +11,7 @@ return {
   ---@type AstroUIOpts
   opts = {
     -- change colorscheme
-    colorscheme = "astrodark",
+    colorscheme = "catppuccin-macchiato",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
@@ -24,16 +24,16 @@ return {
     -- Icons can be configured throughout the interface
     icons = {
       -- configure the loading of the lsp in the status line
-      LSPLoading1 = "⠋",
-      LSPLoading2 = "⠙",
-      LSPLoading3 = "⠹",
-      LSPLoading4 = "⠸",
-      LSPLoading5 = "⠼",
-      LSPLoading6 = "⠴",
-      LSPLoading7 = "⠦",
-      LSPLoading8 = "⠧",
-      LSPLoading9 = "⠇",
-      LSPLoading10 = "⠏",
+      -- LSPLoading1 = "⠋",
+      -- LSPLoading2 = "⠙",
+      -- LSPLoading3 = "⠹",
+      -- LSPLoading4 = "⠸",
+      -- LSPLoading5 = "⠼",
+      -- LSPLoading6 = "⠴",
+      -- LSPLoading7 = "⠦",
+      -- LSPLoading8 = "⠧",
+      -- LSPLoading9 = "⠇",
+      -- LSPLoading10 = "⠏",
     },
   },
 }
