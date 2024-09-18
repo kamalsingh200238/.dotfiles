8db51bccd609fc1ee0a60c42e02a5189acf723b5
Kamal Singh <kamal.singh@exa.net.uk>
2024-09-18 20:31:53 +0100

    add color column after 80

--- nvim/.config/nvim/lua/plugins/astrocore.lua
+++ nvim/.config/nvim/lua/plugins/astrocore.lua
@@ -32,6 +32,7 @@ return {
         spell = false,         -- sets vim.opt.spell
         signcolumn = "yes",    -- sets vim.opt.signcolumn to auto
         wrap = false,          -- sets vim.opt.wrap
+        colorcolumn = '80',
       },
       g = {                    -- vim.g.<key>
         -- configure global vim variables (vim.g)
