7656ee78374dc3168e93ea673500409ee8775b1b
Kamal Singh <kamalsingh200238@gmail.com>
2025-02-02 13:09:27 +0000

    add json, sql, toml support

--- lazyvim/.config/lazyvim/lua/config/lazy.lua
+++ lazyvim/.config/lazyvim/lua/config/lazy.lua
@@ -27,6 +27,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.vue" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.sql" },
+    { import = "lazyvim.plugins.extras.lang.toml" },
 
     -- import/override with your plugins
     { import = "plugins" },
