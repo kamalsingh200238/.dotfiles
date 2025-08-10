10625ef86e5980cc641d891a20017e1c4ecf9d08
Kamal Singh <kamal.singh@exa.net.uk>
2025-08-10 16:43:11 +0100

    add svelte packages

--- lazyvim/.config/lazyvim/lua/config/lazy.lua
+++ lazyvim/.config/lazyvim/lua/config/lazy.lua
@@ -25,6 +25,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.vue" },
+    { import = "lazyvim.plugins.extras.lang.svelte" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.json" },
