15d3c0635eb44b484961245bff9419199da0fcb7
Kamal Singh <kamal.singh@exa.net.uk>
2025-10-09 09:57:51 +0100

    add c# support

--- lazyvim/.config/lazyvim/lua/config/lazy.lua
+++ lazyvim/.config/lazyvim/lua/config/lazy.lua
@@ -26,6 +26,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.vue" },
     { import = "lazyvim.plugins.extras.lang.svelte" },
+    { import = "lazyvim.plugins.extras.lang.omnisharp" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.json" },
