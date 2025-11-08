31e87cd5b01f162969e9864f2c5bb478738887fb
Kamal Singh <kamal.singh@exa.net.uk>
2025-11-08 23:57:21 +0000

    fix lsp and formatter code

--- lazyvim/.config/lazyvim/lua/plugins/lsp.lua
+++ lazyvim/.config/lazyvim/lua/plugins/lsp.lua
@@ -1,11 +1,16 @@
 return {
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       local sources = {
+        "clang-format",
         "clangd",
+        "css-lsp",
+        "emmet-ls",
+        "html-lsp",
         "prettierd",
+        "templ",
       }
       for _, value in ipairs(sources) do
         table.insert(opts.ensure_installed, value)
@@ -16,10 +21,8 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        html = { init_options = { provideFormatter = false } },
         cssls = { init_options = { provideFormatter = false } },
-        emmet_ls = {},
-        templ = {},
+        html = { init_options = { provideFormatter = false } },
       },
     },
   },
