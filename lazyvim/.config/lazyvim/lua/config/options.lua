3cae3f907a7ed598a8abbf07eb5441fec246bf98
Kamal Singh <kamal.singh@exa.net.uk>
2025-11-27 13:41:33 +0000

    use 4 tabs instead of 2

--- lazyvim/.config/lazyvim/lua/config/options.lua
+++ lazyvim/.config/lazyvim/lua/config/options.lua
@@ -1,7 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
---
+
 vim.g.autoformat = false
 vim.g.snacks_animate = false
 vim.opt.colorcolumn = "120"
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
