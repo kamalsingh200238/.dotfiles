1f1222d696e2b06c4bea5e84cb5e34765c9be48e
Kamal Singh <kamal.singh@exa.net.uk>
2026-01-22 22:39:43 +0000

    remove sqlfluff lint config

--- lazyvim/.config/lazyvim/lua/plugins/lint.lua
+++ lazyvim/.config/lazyvim/lua/plugins/lint.lua
@@ -2,12 +2,6 @@ return {
   "mfussenegger/nvim-lint",
   opts = {
     linters = {
-      sqlfluff = {
-        args = {
-          "lint",
-          "--format=json",
-        },
-      },
       ["markdownlint-cli2"] = {
         args = { "--config", '{"MD013":false,"MD031":false}' },
       },
