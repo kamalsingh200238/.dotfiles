5cf44a74f867ac6d647f18d1b497d451d314f4f3
Kamal Singh <kamalsingh200238@gmail.com>
2026-07-17 14:05:00 +0100

    fix indentation in fish pnpm path config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -34,6 +34,6 @@ end
 # pnpm
 set -gx PNPM_HOME "$HOME/Library/pnpm"
 if not string match -q -- "$PNPM_HOME/bin" $PATH
-  set -gx PATH "$PNPM_HOME/bin" $PATH
+    set -gx PATH "$PNPM_HOME/bin" $PATH
 end
 # pnpm end
